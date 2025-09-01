d535dff5d67d9c8d8ca73f5a2c046a23ca365a75
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2025-09-01 11:41:04 +0600

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,18 +17,41 @@ app.use(cookieParser());
 //   credentials: true,
 // }));
 
+// app.use(
+//   cors({
+//     origin: ['*'],
+//     // origin: [
+//     //   'http://localhost:3000',
+//     //   'https://louizmuzik-client.vercel.app',
+//     // ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+//     allowedHeaders: ['Content-Type', 'Authorization'],
+//   })
+// );
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://louizmuzik-client.vercel.app'
+];
+
 app.use(
   cors({
-    origin: [
-      'http://localhost:3000',
-      'https://louizmuzik-client.vercel.app',
-      'https://louizmuzik-client.vercel.app'
-    ],
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
 
 
 // Raw parser BEFORE express.json (Paypal)
