30b4de8ae818bd96b93d99af73c1d12b621c4ae1
kamranhossaintopu <hossainarman953@gmail.com>
2025-11-11 12:29:55 +0600

    "final pdf "

--- src/app/Modules/Melody/melody.controller.ts
+++ src/app/Modules/Melody/melody.controller.ts
@@ -316,7 +316,10 @@ const melodyLicensePdfGenerate = catchAsync(async (req, res) => {
     try {
         const html = await ejs.renderFile(templatePath, data);
 
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+        });
         const page = await browser.newPage();
         await page.setContent(html, { waitUntil: 'networkidle0' });
         const pdfBuffer = await page.pdf({ format: 'A4' });
